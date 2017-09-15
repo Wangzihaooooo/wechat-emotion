7b8d29bdc3757f99473b7958834e5ccfa671ced5
wangzihaooooo <hhhjy5201314@gmail.com>
2017-09-15 19:05:31 +0800

    第二版本

---
+++ src/com/module/recognition/service/Impl/TagServiceImpl.java
@@ -0,0 +1,8 @@
+package com.module.recognition.service.Impl;
+
+/**
+ * description:
+ * Created on 2017/9/15 14:35
+ **/
+public class TagServiceImpl {
+}
---
+++ src/com/module/recognition/service/TagService.java
@@ -0,0 +1,8 @@
+package com.module.recognition.service;
+
+/**
+ * description:
+ * Created on 2017/9/15 14:34
+ **/
+public interface TagService {
+}
